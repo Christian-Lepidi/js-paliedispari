ac2e2413c5ca6c70b30db871d18a6fac20227479
Christian-Lepidi <christianlepidi@gmail.com>
2024-01-17 17:08:10 +0100

    feat: add. js code

--- Pari e dispari/JS/main.js
+++ Pari e dispari/JS/main.js
@@ -1,11 +1,20 @@
 const pariOdispari = prompt("Seleziona pari o dispari");
-const numeroUtente = parseIntprompt("Seleziona un numero da 1 a 5");
+const numeroUtente = parseInt(prompt("Seleziona un numero da 1 a 5"));
 
 function getRandomNumber() {
-  let randomNumber = Math.floor(Math.random * 5 + 1);
+  let randomNumber = Math.floor(Math.random() * 5 + 1);
+  console.log(randomNumber);
   return randomNumber;
 }
 
 const numeroPc = getRandomNumber();
 let risultato = numeroUtente + numeroPc;
 console.log(risultato);
+
+if (pariOdispari == "pari" && risultato % 2 == 0) {
+  console.log("UTENTE VINCE");
+} else if (pariOdispari == "dispari" && risultato % 2 != 0) {
+  console.log("UTENTE VINCE");
+} else {
+  console.log("COMPUTER VINCE");
+}
