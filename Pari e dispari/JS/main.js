8944203d872c8609a170314c990b8de5e5eb3a98
Christian-Lepidi <christianlepidi@gmail.com>
2024-01-18 10:26:26 +0100

    feat: add. changes of js code

--- Pari e dispari/JS/main.js
+++ Pari e dispari/JS/main.js
@@ -1,10 +1,20 @@
 const pariOdispari = prompt("Seleziona pari o dispari");
 const numeroUtente = parseInt(prompt("Seleziona un numero da 1 a 5"));
 
-function getRandomNumber() {
-  let randomNumber = Math.floor(Math.random() * 5 + 1);
-  console.log(randomNumber);
-  return randomNumber;
+if (pariOdispari != "pari" && "dispari") {
+  console.error("I valori non sono corretti");
+}
+if (isNaN(numeroUtente)) {
+  console.error("I valori non sono corretti");
+}
+if (numeroUtente < 1 || numeroUtente > 5) {
+  console.error("I valori non sono corretti");
+} else {
+  function getRandomNumber() {
+    let randomNumber = Math.floor(Math.random() * 5 + 1);
+    console.log(randomNumber);
+    return randomNumber;
+  }
 }
 
 const numeroPc = getRandomNumber();
